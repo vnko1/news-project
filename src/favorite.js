a3446981ac2013c05af8a70fc33b8e25a9c9f3fc
Andrii Valenko <andreyvalenko@gmail.com>
2023-03-11 14:11:37 +0200

    s

--- src/favorite.js
+++ src/favorite.js
@@ -2,7 +2,7 @@ import './scripts/common/current';
 import './scripts/common/mobileMenu';
 import './scripts/common/authMenu';
 import './scripts/common/newsCardFromStorage';
-import './scripts/favoriteBlock';
+import './scripts/favorite/favoriteBlock';
 import './scripts/favorite/themeFavorite';
 import './scripts/favorite/themeFavoriteMobile';
 import './scripts/favorite/searchNewsfromFavoriteStorage';
