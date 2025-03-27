ed88dfad83a336ab26ef69790f0481e3014ee3ec
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2025-03-27 16:23:05 +0200

    changed: firebase api creds

--- src/scripts/firebase/firebaseApi.js
+++ src/scripts/firebase/firebaseApi.js
@@ -1,12 +1,14 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAGhSMKP3vqfl8yduNbTzGe0aFAL5yk6sA',
-  authDomain: 'news-my-project.firebaseapp.com',
-  projectId: 'news-my-project',
-  storageBucket: 'news-my-project.appspot.com',
-  messagingSenderId: '525076045856',
-  appId: '1:525076045856:web:0de49aeeb3a25ec5511c45',
+  apiKey: 'AIzaSyBiqaFh7oDwgmPdj10q37YlwajRZxOefv8',
+  authDomain: 'news-d8f1f.firebaseapp.com',
+  databaseURL:
+    'https://news-d8f1f-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'news-d8f1f',
+  storageBucket: 'news-d8f1f.firebasestorage.app',
+  messagingSenderId: '958128078409',
+  appId: '1:958128078409:web:71529894f3a4a12ddd8998',
 };
 
 export const app = initializeApp(firebaseConfig);
